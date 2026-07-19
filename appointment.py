5797bb3cc9eea82fb59941d7d19bda06d3925616
unknown <h33840228@gmail.com>
2026-07-19 17:15:53 +0530

    update in feature branch

--- appointment.py
+++ appointment.py
@@ -2,3 +2,5 @@ print("Hospital Appointment Booking System")
 print("patient Registration module loaded.")#added line
 def login():
     print("user login system") #added login
+#Error: This is wrong modification that needs to be disacrded!
+print("wrong code")
